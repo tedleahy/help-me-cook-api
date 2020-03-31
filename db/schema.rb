53af7dd7c119fd235f3c288b77c07a423ae14c9e
Ted Leahy <edwardleahy@protonmail.com>
2020-04-01 00:55:30 +0100

    Add source_url attribute to Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_230406) do
+ActiveRecord::Schema.define(version: 2020_03_31_234849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_230406) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "instructions", array: true
+    t.string "source_url"
   end
 
   add_foreign_key "ingredient_recipes", "ingredients"
